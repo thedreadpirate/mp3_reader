d4f9d23d1ebb1df2913a610c7dcbe1f619dccc24
Keith <obrienkeithd@gmail.com>
2013-12-07 10:21:11 -0500

    cache the files in memory for now. change the matching logic

--- server.rb
+++ server.rb
@@ -3,33 +3,20 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
-get '/' do
-	haml :home
-end
-
-get '/files/:filterString?:max?' do
-	filter_by = params[:filterString].downcase
-	max_returned = params[:max].nil? ? 500 : params[:max].to_i
-	read_mp3s.select{|item| include_item?(item, filter_by) }.take(max_returned).to_json
-end
-
-def include_item?(item, filter_string)
-	item[:title].downcase.include?(filter_string) || 
-	item[:artist].downcase.include?(filter_string) || 
-	item[:genre].downcase.include?(filter_string)
-end
-
 def read_mp3s
-	files = Dir['mp3s/*']
+	files = Dir['../../Shared/Music/**/*.mp3']
 
-	mp3_files = Array.new	
+	mp3_files = Array.new
 
 	files.each do |file|
-		TagLib::FileRef.open(file) do |fileref| 
+		TagLib::FileRef.open(file) do |fileref|
 			unless fileref.null?
     			tag = fileref.tag
 
-    			mp3_files << {:path => file, :title => tag.title, :artist => tag.artist, :genre => tag.genre}
+    			mp3_files << {:path => file,
+                        :title => tag.title,
+                        :artist => tag.artist,
+                        :genre => tag.genre}
     		end
   		end
 	end  # File is automatically closed at block end
@@ -39,4 +26,28 @@ def read_mp3s
 	end
 	mp3_files
 end
+
+$mp3_files = read_mp3s
+
+get '/' do
+	haml :home
+end
+
+get '/files/:filterString?:max?' do
+	filter_by = params[:filterString].downcase
+	max_returned = params[:max].nil? ? 500 : params[:max].to_i
+	$mp3_files.select{|item| include_item?(item, filter_by) }.take(max_returned).to_json
+end
+
+def include_item?(item, filter_string)
+	check_value(item[:title], filter_string) ||
+  check_value(item[:artist], filter_string) ||
+  check_value(item[:genre], filter_string)
+end
+
+def check_value(value, string_to_check)
+  value.nil? ? false : value.downcase.include?(string_to_check)
+end
+
+
 
