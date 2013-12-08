79a2a12ec9ff8ad9ce2ae71082f9a7ed0f960482
Keith <obrienkeithd@gmail.com>
2013-12-08 09:04:20 -0500

    update with the latest version of the autocomplete directive

--- public/scripts/autocomplete.js
+++ public/scripts/autocomplete.js
@@ -1,5 +1,5 @@
 angular.module('ngAutocompleteModule', []).
-directive('ngAutocomplete', function($templateCache, $compile){
+directive('ngAutocomplete', function($templateCache, $compile, $timeout){
 	return {
 		restrict: 'EA',
 		scope: {
@@ -13,39 +13,57 @@ directive('ngAutocomplete', function($templateCache, $compile){
             var max_results = attrs.maxResults;
             var min_length = parseInt(attrs.minLength) || 1;
 
-            var base_template = '<div class="ac-wrapper"><input type="text" ng-model="ngModel" />#TEMPLATE#</div>';
-            var simple_template = '<div class="ac-items"><div class="ac-item" ng-click="itemSelected(item)" ng-repeat="item in items">{{ item }}</div></div>';
-            var property_template = '<div class="ac-items"><div class="ac-item" ng-click="itemSelected(item)" ng-repeat="item in items">{{ item["' + value_property + '"] }}</div></div>';
+            scope.getTemplate = function(){
+                if(value_property != undefined){
+                    return '{{ item["' + value_property + '"] }}';
+                }else if(advanced_display != undefined){
+                    return $templateCache.get(advanced_display);
+                }else{
+                    return '{{ item }}';
+                }
+            };
 
-			var retrieve_list = function(newVal, oldVal){
-				return !(oldVal == undefined && newVal == undefined)
-					&& newVal !== '' && newVal.length >= min_length;
-			}
+            scope.onBlur = function(){
+                $timeout(function(){
+                    scope.focussed = false;
+                }, 50);
+            }
 
-			scope.$watch('ngModel', function(newVal, oldVal) {
-				if(retrieve_list(newVal, oldVal)){
-					scope.retrievalMethod()(scope.ngModel, max_results)
-					.then(function(data){
-						scope.items = data;
-					});
-				}else{
-					scope.items = [];
-				}
-			});
+            scope.onFocus = function(){
+                scope.focussed = true;
+            }
 
-			scope.itemSelected = function(item){
-				scope.onSelect()(item);
-			}
+            var retrieve_list = function(newVal, oldVal){
+                return !(oldVal == undefined && newVal == undefined)
+                    && newVal !== '' && newVal.length >= min_length;
+            }
 
-			scope.getTemplate = function(){
-				if(value_property != undefined){
-					return property_template;
-				}else if(advanced_display != undefined){
-					return $templateCache.get(advanced_display);
-				}else{
-					return simple_template;
-				}
-			};
+
+            scope.$watch('ngModel', function(newVal, oldVal) {
+                if(retrieve_list(newVal, oldVal)){
+                    scope.retrievalMethod()(scope.ngModel, max_results)
+                        .then(function(data){
+                            scope.items = data;
+                        });
+                }else{
+                    scope.items = [];
+                }
+            });
+
+            scope.itemSelected = function(item){
+                scope.onSelect()(item);
+            }
+
+            var base_template = '<div class="ac-wrapper" style="width:inherit">' +
+                                '    <input type="text" style="width:inherit" ng-model="ngModel" ng-blur="onBlur()" ng-focus="onFocus()" />' +
+                                '    <div class="ac-items" style="width:inherit" ng-show="focussed">' +
+                                '       <div class="ac-item" style="width:inherit" ng-click="itemSelected(item)" ng-repeat="item in items">' +
+                                            scope.getTemplate() +
+                                '        </div>' +
+                                '    </div>' +
+                                '</div>';
+
+
             element.html(base_template.replace('#TEMPLATE#', scope.getTemplate()).trim());
             $compile(element.contents())(scope);
 
