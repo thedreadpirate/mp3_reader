21ba2032b66948919f181fff74b9d2eadb6463c8
ko <collateralwebteam@us.ibm.com>
2013-12-09 17:14:24 -0500

    allow a local playlist to be built

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -13,20 +13,60 @@ mp3Reader.factory('mp3service', function($http){
 	};
 });
 
+mp3Reader.directive('ngAudio', [function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// cont­rol­ler: function($scope, $element, $attrs, $transclue) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, element, iAttrs, controller) {
+			element.bind('ended', $scope.setSong);	
+		}
+	};
+}]);
+
 mp3Reader.controller('homeCtrl', function($scope, $timeout, mp3service){
+	$scope.selected = [];
+	$scope.playing = false;
 
     $scope.getFiles = function(input, max_results){
     	return mp3service.getFileList(input, max_results);
     }
 
-	$scope.clicked = function(selected){
-		$scope.selected = selected;
+    $scope.setSong = function(){
+    	$scope.selected.splice(0,1);
+    	var selected = $scope.selected[0];
+
         $scope.currently_playing = selected.path;
+        
         $timeout(function(){
-            var audio = document.getElementById('audio');
-            audio.load();
-            audio.play();
-        }, 250);
+    	        var audio = document.getElementById('audio');
+	            audio.load();
+            	audio.play();
+            	$scope.playing = true;
+        	}, 250);
+
+    }
+	$scope.clicked = function(selected){
+		$scope.selected.push(selected);
+        $scope.currently_playing = selected.path;
+        if(!$scope.playing){
+        $timeout(function(){
+    	        var audio = document.getElementById('audio');
+	            audio.load();
+            	audio.play();
+            	$scope.playing = true;
+        	}, 250);
+    	}
 	};
 
 });
