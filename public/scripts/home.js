7c39f018eabe91fb23502a89f2b624c0f01d7959
Keith <obrienkeithd@gmail.com>
2013-12-09 18:23:09 -0500

    not working yet

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -13,60 +13,71 @@ mp3Reader.factory('mp3service', function($http){
 	};
 });
 
-mp3Reader.directive('ngAudio', [function(){
+mp3Reader.directive('ngAudio', ['$timeout', function($timeout){
 	// Runs during compile
 	return {
 		// name: '',
 		// priority: 1,
 		// terminal: true,
-		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+//	    scope: {
+//            selectedSongs: '='
+//        }, // {} = isolate, true = child, false/undefined = no change
 		// cont­rol­ler: function($scope, $element, $attrs, $transclue) {},
-		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        replace: true,
 		// template: '',
 		// templateUrl: '',
-		replace: true,
+        template: '<div><audio controls>' +
+                  '    <source src="{{ currently_playing }}" /> Your browser does not support the audio element. ' +
+                  '</audio>' +
+                  '<div> Now playing: {{playlist[0].title}} by {{ playlist[0].artist }}</div>' +
+                  '<div> Upcoming:' +
+                  '   <div ng-repeat="item in playlist" ng-if="$index !== 0"></div>' +
+                  '</div></div>',
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-		link: function($scope, element, iAttrs, controller) {
-			element.bind('ended', $scope.setSong);	
+		link: function($scope, element, attrs) {
+            var audio = element.find('audio');
+
+			element.bind('ended', $scope.setSong);
+
+            var playlist = [];
+
+            $scope.$watch('selected', function(newVal, oldVal){
+                if(newVal !== undefined && newVal !== oldVal){
+                    playlist.push(newVal);
+                    setSong();
+                }
+            }, true);
+
+            var setSong = function(){
+
+
+                $timeout(function(){
+                    audio.load();
+                    audio.play();
+                    playing = true;
+                }, 250);
+            }
 		}
 	};
 }]);
 
 mp3Reader.controller('homeCtrl', function($scope, $timeout, mp3service){
-	$scope.selected = [];
-	$scope.playing = false;
 
+    $sc
     $scope.getFiles = function(input, max_results){
     	return mp3service.getFileList(input, max_results);
     }
 
-    $scope.setSong = function(){
-    	$scope.selected.splice(0,1);
-    	var selected = $scope.selected[0];
-
-        $scope.currently_playing = selected.path;
-        
-        $timeout(function(){
-    	        var audio = document.getElementById('audio');
-	            audio.load();
-            	audio.play();
-            	$scope.playing = true;
-        	}, 250);
-
-    }
 	$scope.clicked = function(selected){
-		$scope.selected.push(selected);
-        $scope.currently_playing = selected.path;
-        if(!$scope.playing){
-        $timeout(function(){
-    	        var audio = document.getElementById('audio');
-	            audio.load();
-            	audio.play();
-            	$scope.playing = true;
-        	}, 250);
-    	}
-	};
+		$scope.selected = selected;
 
+        var removed = playlist.splice(0,1);
+        playlist.push(removed);
+
+        var selected = playlist[0];
+
+        $scope.currently_playing = selected.path;
+	};
 });
